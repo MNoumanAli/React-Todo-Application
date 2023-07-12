6d58e73cfe6ee07c951cd7da79e3b43994ab4bc7
MNoumanAli <mnoumanali001@gmail.com>
2023-07-12 23:32:38 +0500

    Login/Signup Form

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+// import { login } from '../API'
 
-const Login = () => {
-    return (
-        <div>
-            
+export default function Login()
+{
+    let [data, changeData] = useState({email : "" , password: ""})
+    // const navigate = useNavigate()
+    function changeOccure(e)
+    {
+        changeData(d => {
+            return {
+                ...data,
+                [e.target.name]: e.target.value
+            }
+        })
+    }
+    function loginUser(e)
+    {
+        // e.preventDefault()
+        // try{
+        //     login(data)
+        //     .then(res => {
+        //         localStorage.setItem("user" , JSON.stringify(res.data))
+        //         navigate("/home")
+        //     })
+        // }catch(err)
+        // {
+        //     console.log(err)
+        // }
+    }
+    return(
+        <div className='form'>
+            <div className='title'>
+                <h2>Login </h2>
+            </div>
+            <form onSubmit={loginUser}>
+                <input type="email" placeholder='Email' name='email' onChange={changeOccure} className="input"/>
+                <input type="password" placeholder= "Password" name='password' onChange={changeOccure} className="input"/>
+                <button type="submit" className='opt-1 login-btn'>Login</button>
+            </form>
         </div>
-    );
-};
-
-export default Login;
+    )
+}
--- client/src/Components/Signup.jsx
+++ client/src/Components/Signup.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React from 'react'
+import { useState } from 'react'
+// import {  useNavigate } from 'react-router-dom'
+// import { signup } from '../API'
 
-const Signup = () => {
-    return (
-        <div>
-            
+export default function Signup()
+{
+    // const navigate = useNavigate()
+    let [data, changeData] = useState({email : "" , password: "", username: ""})
+    function changeOccure(e)
+    {
+        changeData(d => {
+            return {
+                ...data,
+                [e.target.name]: e.target.value
+            }
+        })
+    }
+    function signupUser(e)
+    {
+        e.preventDefault()
+        // signup(data).then(()=> navigate('/login'))
+    }
+    return(
+        <div className='form'>
+            <div className='title'>
+                <h2>SignUp</h2>
+            </div>
+            <form onSubmit={signupUser}>
+                <input type="email" placeholder='Email' name='email' onChange={changeOccure}/>
+                <input type="password" placeholder= "Password" name='password' onChange={changeOccure}/>
+                <input type= "text" placeholder='username' name='username' onChange={changeOccure}/>
+                <button type="submit" className='login-btn opt-1'>Login</button>
+            </form>
         </div>
-    );
-};
-
-export default Signup;
+    )
+}
