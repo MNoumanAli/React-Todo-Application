e184582db13ae2d5278b14bf2619024aa38b001d
MNoumanAli <mnoumanali001@gmail.com>
2023-07-30 17:25:07 +0500

    Todo Model

---
+++ Server/models/todo.js
@@ -0,0 +1,21 @@
+
+import mongoose from "mongoose";
+
+const todoSchema = mongoose.Schema({
+    title: String,
+    status: {
+        type: Boolean,
+        default: false
+    },
+    createdDate: {
+        type: Date,
+        default: new Date()
+    },
+    owner : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "owner"
+    }
+})
+
+const todo = mongoose.model("todo" , todoSchema)
+export default todo
