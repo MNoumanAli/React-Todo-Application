2685cdedf522066caee04763a4263c9720166f00
MNoumanAli <mnoumanali001@gmail.com>
2023-07-30 17:24:45 +0500

    Model

---
+++ Server/models/employee.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import owner from "./owner.js";
+
+const schema = mongoose.Schema({
+    email : String,
+    name : String,
+    owner : [{ type: mongoose.Schema.Types.ObjectId, ref: 'owner' }]
+})
+
+const employee = mongoose.model( 'employee' , schema)
+export default employee
